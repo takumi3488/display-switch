2cc39d8a83fdbe4934d4e1337b4c6677f16bbc3a
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2024-07-07 12:33:22 +0900

    ⬆️ (Cargo.toml): upgrade regex dependency to version 1.10.5 ♻️ (Makefile): refactor target dependencies to include /opt/homebrew/bin/displayplacer 📝 (main.rs): update import statements for io, stdout, and Write 📝 (placer.rs): update import statements for process::Command and str::from_utf8 📝 (placer.rs): refactor set method to accept places as a vector of strings and handle output errors 📝 (store.rs): add Clone trait to Place struct 📝 (store.rs): add Clone trait to Display struct 📝 (store.rs): add get_id method to Place struct 📝 (store.rs): add get_places and get_places_vec methods to Display struct 📝 (store.rs): add places_from_str method to Display struct 📝 (store.rs): update Display struct to store places as a vector of Place structs 📝 (store.rs): update Display struct to accept places as a reference to a vector of Place structs 📝 (store.rs): update Display struct to create a new Display with a reference to a vector of Place structs 📝 (store.rs): update Display struct to get places as a vector of strings 📝 (store.rs): update Display struct to get places from a string representation 📝 (store.rs): update DisplaySwitch struct to add Clone trait 📝 (store.rs): update DisplaySwitch struct to add save method to serialize displays to a file 📝 (store.rs): update DisplaySwitch struct to add add method to push a cloned display to displays and save 📝 (store.rs): update DisplaySwitch struct to initialize displays with an empty vector if config file does not exist

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod placer;
 mod store;
-use std::io;
+use std::io::{self, stdout, Write};
 
 use placer::Placer;
 use store::{Display, DisplaySwitch};
@@ -8,31 +8,54 @@ use store::{Display, DisplaySwitch};
 fn main() {
     let mut display_switch = DisplaySwitch::new();
     let placer = Placer::new();
-    let current: &str = &placer.current();
+    let current = Display::places_from_str(&placer.current());
 
     if display_switch
         .displays
         .iter()
-        .find(|&d| d.get_place() == current)
+        .find(|&d| d.get_places() == &current)
         .is_none()
     {
         print!("Current display place is not in the list. Enter a name for the current display: ");
+        stdout().flush().unwrap();
         let mut name = String::new();
         io::stdin().read_line(&mut name).unwrap();
         if name.trim().is_empty() {
-            println!("Name cannot be empty.");
+            print!("Name cannot be empty. Continue without saving? (y/n): ");
+            stdout().flush().unwrap();
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            if input.trim() != "y" {
+                return;
+            }
+        } else {
+            let current_display = Display::new(name.trim(), &current);
+            display_switch.add(&current_display);
+            placer.set(&current_display.get_places_vec());
+            println!("Added display: {}", current_display.get_name());
             return;
         }
-        display_switch.add(Display::new(name.trim().to_string(), current.to_string()));
     }
 
     let current_index = display_switch
         .displays
         .iter()
-        .position(|d| d.get_place() == current)
-        .unwrap();
-    let next_index = (current_index + 1) % display_switch.displays.len();
-    let next = &display_switch.displays[next_index];
-    placer.set(next.get_place());
+        .position(|d| d.get_places() == &current)
+        .unwrap_or(display_switch.displays.len() - 1);
+    let mut current_place_ids = current.iter().map(|p| p.get_id()).collect::<Vec<_>>();
+    current_place_ids.sort();
+    let next_index = (1..=display_switch.displays.len())
+        .find(|i| {
+            let places = &display_switch.displays
+                [(current_index + i) % display_switch.displays.len()]
+            .get_places();
+            let mut place_ids = places.iter().map(|p| p.get_id()).collect::<Vec<_>>();
+            place_ids.sort();
+            place_ids == current_place_ids
+        })
+        .expect("No display place found for current display.");
+    let next =
+        &display_switch.displays[(current_index + next_index) % display_switch.displays.len()];
+    placer.set(&next.get_places_vec());
     println!("Switched to display: {}", next.get_name());
 }
--- src/store.rs
+++ src/store.rs
@@ -1,25 +1,97 @@
 use std::fs::File;
 
 use dir::home_dir;
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Display {
     name: String,
-    place: String,
+    places: Vec<Place>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct Place {
+    id: String,
+    res: (u32, u32),
+    hz: f32,
+    color_depth: u32,
+    enabled: bool,
+    scaling: String,
+    origin: (i32, i32),
+    degree: i32,
+}
+
+impl Place {
+    pub fn get_id(&self) -> &str {
+        &self.id
+    }
 }
 
 impl Display {
-    pub fn new(name: String, place: String) -> Display {
-        Display { name, place }
+    pub fn new(name: &str, places: &Vec<Place>) -> Display {
+        Display {
+            name: name.to_string(),
+            places: places.clone(),
+        }
     }
 
     pub fn get_name(&self) -> &str {
         &self.name
     }
 
-    pub fn get_place(&self) -> &str {
-        &self.place
+    pub fn get_places(&self) -> &Vec<Place> {
+        &self.places
+    }
+
+    pub fn get_places_vec(&self) -> Vec<String> {
+        self.places
+                .iter()
+                .map(|p| {
+                    format!(
+                        "id:{} res:{} hz:{} color_depth:{} enabled:{} scaling:{} origin:{} degree:{}",
+                        p.id,
+                        format!("{}x{}", p.res.0, p.res.1),
+                        p.hz,
+                        p.color_depth,
+                        p.enabled,
+                        p.scaling,
+                        format!("({},{})", p.origin.0, p.origin.1),
+                        p.degree
+                    )
+                })
+                .collect::<Vec<_>>()
+    }
+
+    pub fn places_from_str(s: &str) -> Vec<Place> {
+        let ptn = Regex::new(r#"id:(?<id>[0-9A-Z]{8}-([0-9A-Z]{4}-){3}[0-9A-Z]{12}) res:(?<res>[0-9]+x[0-9]+) hz:(?<hz>[0-9.]+) color_depth:(?<color_depth>[0-9]+) enabled:(?<enabled>true|false) scaling:(?<scaling>\S+) origin:(?<origin>\(-?[0-9]+,-?[0-9]+\)) degree:(?<degree>-?[0-9]+)"#).unwrap();
+        s.split("\" \"")
+            .map(|s| s.replace("\"", ""))
+            .map(|s| {
+                let caps = ptn
+                    .captures(&s)
+                    .expect("Unssupported format from displayplacer");
+                Place {
+                    id: caps["id"].to_string(),
+                    res: {
+                        let res: Vec<&str> = caps["res"].split("x").collect();
+                        (res[0].parse().unwrap(), res[1].parse().unwrap())
+                    },
+                    hz: caps["hz"].parse().unwrap(),
+                    color_depth: caps["color_depth"].parse().unwrap(),
+                    enabled: caps["enabled"] == *"true",
+                    scaling: caps["scaling"].to_string(),
+                    origin: {
+                        let origin: Vec<&str> = caps["origin"]
+                            .trim_matches(|c| c == '(' || c == ')')
+                            .split(',')
+                            .collect();
+                        (origin[0].parse().unwrap(), origin[1].parse().unwrap())
+                    },
+                    degree: caps["degree"].parse().unwrap(),
+                }
+            })
+            .collect::<Vec<_>>()
     }
 }
 
@@ -31,8 +103,10 @@ pub struct DisplaySwitch {
 impl DisplaySwitch {
     pub fn new() -> DisplaySwitch {
         let config_file_path = home_dir().unwrap().join(".display-switch.json");
-        let config_file =
-            File::open(&config_file_path).unwrap_or(File::create(&config_file_path).unwrap());
+        let config_file = match File::open(&config_file_path) {
+            Ok(file) => file,
+            Err(_) => File::create(&config_file_path).unwrap(),
+        };
         let displays: Vec<Display> = serde_json::from_reader(config_file).unwrap_or(vec![]);
         DisplaySwitch { displays }
     }
@@ -40,11 +114,11 @@ impl DisplaySwitch {
     fn save(&self) {
         let config_file_path = home_dir().unwrap().join(".display-switch.json");
         let config_file = File::create(&config_file_path).unwrap();
-        serde_json::to_writer(config_file, self).unwrap();
+        serde_json::to_writer_pretty(config_file, &self.displays).unwrap();
     }
 
-    pub fn add(&mut self, display: Display) {
-        self.displays.push(display);
+    pub fn add(&mut self, display: &Display) {
+        self.displays.push(display.clone());
         self.save();
     }
 }
